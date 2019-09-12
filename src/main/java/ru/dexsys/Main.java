b4b9afb74d4fb9d69665fe0e56112b160cd69469
alekssanta <alekslapin@bk.ru>
2019-09-12 16:02:38 +0400

    12 09 2019

--- src/main/java/ru/dexsys/Main.java
+++ src/main/java/ru/dexsys/Main.java
@@ -1,18 +1,18 @@
 package ru.dexsys;
 
 import java.net.URI;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
+
     private static final String USERNAME = "dexautomation";
     private static final String PASSWORD = "dexautomation";
     private static final String URL = "jdbc:mysql://db4free.net:3306/dexautomation";
     //"jdbc:mysql://localhost:3306/mysql?useSSL=false&serverTimezone=UTC";
+
+
     public static void main(String[] args) throws SQLException {
-        Connection connection = null;
+
         Driver driver;
         try {
             driver = new com.mysql.cj.jdbc.Driver();
@@ -29,16 +29,32 @@ public class Main {
             return;
         }
         try {
-            connection = DriverManager.getConnection(URL,USERNAME, PASSWORD);
-            System.out.println("code... ");
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); // подключаемся к бд
+            Statement statement = connection.createStatement();
+
+            statement.execute("insert into Students (firstName, lastName, age, phone) values (\"Alexandr\", \"Makedonsky\", \"2375\", null )");
+// создаем пункт в таблице с данными
+            ResultSet resultSet = statement.executeQuery("select *from Students");
+            while (resultSet.next()) {
+                // смотрим поля в таблице
+                System.out.println(resultSet.getInt(1));
+                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getString(3));
+                System.out.println(resultSet.getString(4));
+                System.out.println(resultSet.getString(5));
+                System.out.println("___________");  //разделитель
+
+
+            }
         }
+
          catch (SQLException ex){
              System.out.println("не удалось соединение");
              return;
          }
-finally {
-            if (connection !=null)
-                connection.close();
-        }
+//finally {
+//            if (connection !=null)
+//                connection.close();
+//        }
     }
 }
